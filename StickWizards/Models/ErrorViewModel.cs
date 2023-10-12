9cda262a387136fd338bde973e76add9834cc3cb
nidhipatel0318 <135552374+nidhipatel0318@users.noreply.github.com>
2023-10-12 11:57:39 -0400

    Add project files.

---
+++ StickWizards/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StickWizards.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
